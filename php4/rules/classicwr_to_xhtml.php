a311affe9fd379eca2aacd30b3c13609f83b71c9
Laurent Jouanneau <laurent@jelix.org>
2006-10-08 21:38:55 +0000

    modif mineure sur la licence
    --HG--
    extra : convert_revision : svn%3A15610f44-3b06-0410-94f7-ce48b8216d56/trunk%4036

--- php4/rules/classicwr_to_xhtml.php
+++ php4/rules/classicwr_to_xhtml.php
@@ -9,9 +9,8 @@
  * @link http://wikirenderer.berlios.de
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * modify it under the terms of the GNU Lesser General Public 2.1
+ * License as published by the Free Software Foundation.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- php4/rules/wr3_to_xhtml.php
+++ php4/rules/wr3_to_xhtml.php
@@ -10,7 +10,7 @@
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public 2.1
- * License as published by the Free Software Foundation
+ * License as published by the Free Software Foundation.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
