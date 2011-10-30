26d35ddb353df0177cdf465c3013e504346fbb56
Laurent Jouanneau <laurent@jelix.org>
2011-10-30 13:53:29 +0100

    new host migration #3: links in sources

--- src/WikiTag.class.php
+++ src/WikiTag.class.php
@@ -4,7 +4,7 @@
  * @package WikiRenderer
  * @author Laurent Jouanneau
  * @copyright 2003-2008 Laurent Jouanneau
- * @link http://wikirenderer.berlios.de
+ * @link http://wikirenderer.jelix.org
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public 2.1
