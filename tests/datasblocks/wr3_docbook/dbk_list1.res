abeddcc40da3824ce2aac078744e4a8f21e00385
Laurent Jouanneau <dev@ljouanneau.com>
2016-02-06 21:30:30 +0100

    new tests wr3 to docbook

---
+++ tests/datasblocks/wr3_docbook/dbk_list1.res
@@ -0,0 +1,49 @@
+<para>Lorem <emphasis role="strong">ipsum dolor</emphasis> sit amet, <emphasis>consectetuer adipiscing</emphasis> elit. Ut scelerisque. Ut iaculis ultrices nulla. Cras viverra diam nec justo.</para>
+
+<itemizedlist>
+<listitem><para>Phasellus non eros sit amet sem tristique laoreet.</para>
+<orderedlist>
+<listitem><para>Nam mi wisi, pellentesque dictum,</para></listitem>
+<listitem><para>tristique in, tristique quis, erat.</para>
+<orderedlist>
+<listitem><para>In in erat ut urna vulputate vestibulum. Aenean justo.</para></listitem>
+<listitem><para>In quis nisl.</para></listitem></orderedlist>
+</listitem></orderedlist>
+</listitem>
+<listitem><para>Morbi justo libero, pharetra a,</para></listitem>
+<listitem><para>malesuada eget, lacinia in, ligula.</para></listitem></itemizedlist>
+
+<para>Mauris <link xlink:href="http://ljouanneau.com">sit amet massa</link> pretium dapibus.</para>
+
+
+<itemizedlist>
+<listitem>
+<itemizedlist>
+<listitem>
+<itemizedlist>
+<listitem><para>Phasellus non eros sit amet </para></listitem>
+<listitem><para>sem tristique laoreet.</para></listitem></itemizedlist>
+</listitem></itemizedlist>
+
+<orderedlist>
+<listitem><para>Nam mi wisi, pellentesque dictum,</para></listitem>
+<listitem><para>tristique in, tristique quis, erat.</para>
+<orderedlist>
+<listitem><para>In in erat ut urna vulputate vestibulum. Aenean justo.</para></listitem>
+<listitem><para>In quis nisl.</para></listitem></orderedlist>
+</listitem></orderedlist>
+</listitem>
+<listitem><para>Morbi justo libero, pharetra a,</para></listitem>
+<listitem><para>malesuada eget, lacinia in, ligula.</para></listitem></itemizedlist>
+
+<para>other</para>
+
+<itemizedlist>
+<listitem><para>Phasellus non eros sit amet sem tristique laoreet.</para>
+<itemizedlist>
+<listitem><para>Nam mi wisi, pellentesque dictum,</para></listitem>
+<listitem><para>tristique in, tristique quis, erat.</para></listitem></itemizedlist>
+</listitem></itemizedlist>
+<orderedlist>
+<listitem><para>In in erat ut urna vulputate vestibulum. Aenean justo.</para></listitem>
+<listitem><para>In quis nisl.</para></listitem></orderedlist>
--- tests/wr3_dbk_blocksTest.php
+++ tests/wr3_dbk_blocksTest.php
@@ -12,22 +12,27 @@ class WR3DBKTestsBlocks extends PHPUnit_Framework_TestCase {
 
     function getListblocks() {
         return array(
-            array('para1','dbk_para1',0),
-            array('para2','dbk_para2',0),
-            array('wr3/wr3_list1',   'dbk_wr3_list1',0),
-            array('wr3/wr3_pre',     'dbk_wr3_pre',0),
-            array('wr3/wr3_footnote','dbk_wr3_footnote',0),
+            array('dbk_para1',0),
+            array('dbk_para2',0),
+            array('dbk_list1',0),
+            array('dbk_wr3_pre',0),
+            //array('wr3/wr3_footnote',0),
         );
     }
 
     /**
      * @dataProvider getListblocks
      */
-    function testBlock($file, $file2, $nberror) {
+    function testBlock($file, $nberror) {
 
-        $wr = new \WikiRenderer\Renderer(new \WikiRenderer\Markup\WR3DocBook\Config());
-        $sourceFile = 'datasblocks/'.$file.'.src';
-        $resultFile = 'datasblocks/'.$file2.'.res';
+        $genConfig = new \WikiRenderer\Generator\Docbook\Config();
+        $generator = new \WikiRenderer\Generator\Docbook\Document($genConfig);
+
+        $config = new \WikiRenderer\Markup\WR3\Config();
+
+        $wr = new \WikiRenderer\RendererNG($generator, $config);
+        $sourceFile = 'datasblocks/wr3_docbook/'.$file.'.src';
+        $resultFile = 'datasblocks/wr3_docbook/'.$file.'.res';
 
         $source = file_get_contents($sourceFile);
         $expected = file_get_contents($resultFile);
@@ -36,6 +41,7 @@ class WR3DBKTestsBlocks extends PHPUnit_Framework_TestCase {
 
         $this->assertEquals($expected, $res);
         $this->assertEquals($nberror, count($wr->errors));
+        $this->validateDocbook($res);
     }
 
     function getListblocks2() {
@@ -49,11 +55,16 @@ class WR3DBKTestsBlocks extends PHPUnit_Framework_TestCase {
     /**
      * @dataProvider getListblocks2
      */
-    function testBlock2($file, $nberror) {
+/*    function testBlock2($file, $nberror) {
 
-        $wr = new \WikiRenderer\Renderer(new \WikiRenderer\Markup\WR3DocBook\Config());
-        $sourceFile = 'datasblocks/'.$file.'.src';
-        $resultFile = 'datasblocks/'.$file.'.res';
+        $genConfig = new \WikiRenderer\Generator\Docbook\Config();
+        $generator = new \WikiRenderer\Generator\Docbook\Document($genConfig);
+
+        $config = new \WikiRenderer\Markup\WR3\Config();
+
+        $wr = new \WikiRenderer\RendererNG($generator, $config);
+        $sourceFile = 'datasblocks/wr3_docbook/'.$file.'.src';
+        $resultFile = 'datasblocks/wr3_docbook/'.$file.'.res';
 
         $source = file_get_contents($sourceFile);
         $expected = file_get_contents($resultFile);
@@ -62,7 +73,31 @@ class WR3DBKTestsBlocks extends PHPUnit_Framework_TestCase {
 
         $this->assertEquals($expected, $res);
         $this->assertEquals($nberror, count($wr->errors));
+        $this->validateDocbook($res);
     }
+*/
+    protected function validateDocbook($res) {
+        $docbook_rng = getenv('DOCBOOK_RNG');
+        if ($docbook_rng) {
+            $relaxng = '/usr/share/xml/docbook/schema/rng/5.0/docbook.rng';
+            if (strpos($docbook_rng, '.rng') !== false ) {
+                $relaxng = $docbook_rng;
+            }
+            $docbook = '<'."?xml version='1.0'?>\n";
+            $docbook .= '<book xmlns="http://docbook.org/ns/docbook" xmlns:xlink="http://www.w3.org/1999/xlink" version="5.0">
+<title>My First Book</title><article><title>test</title>
+';
+            $docbook .= $res;
+            $docbook .= '</article></book>';
 
+            file_put_contents('docbook_result.xml', $docbook);
+            $output = array();
+            $returnCode = 0;
+            $cmd = "xmllint --relaxng ".escapeshellcmd($relaxng).' --noout docbook_result.xml 2>&1';
+            exec($cmd, $output, $returnCode);
+            $this->assertEquals("docbook_result.xml validates", implode("\n", $output));
+        }
+
+    }
 
 }
