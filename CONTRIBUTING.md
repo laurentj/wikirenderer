391246f1d909e90e38198a5b2ed2952229ef4468
Laurent Jouanneau <dev@ljouanneau.com>
2016-02-06 01:05:19 +0100

    Docbook test: adds relaxng validation

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -14,3 +14,13 @@ When you do modifications in the source code, be sure that all tests pass:
 cd tests
 ../vendor/bin/phpunit
 ```
+
+
+
+Tests about docbook can also validate generated docbook content against
+the official Docbook 5 relaxng schema. If you want to activate validation tests,
+install xmllint, docbook 5 relaxng, and create an environment variable
+DOCBOOK_RNG containing "yes" or a path to the relaxng file.
+
+On debian/ubuntu, you just have to install libxml2-utils and docbook5-xml
+packages, and set DOCBOOK_RNG="yes".
--- tests/dokuwiki_docbook_blocksTest.php
+++ tests/dokuwiki_docbook_blocksTest.php
@@ -95,6 +95,28 @@ truc3 </programlisting>',
 
         $this->assertEquals($result, $res);
         $this->assertEquals($nberror, count($wr->errors));
+
+        $docbook_rng = getenv('DOCBOOK_RNG');
+        if ($docbook_rng) {
+            $relaxng = '/usr/share/xml/docbook/schema/rng/5.0/docbook.rng';
+            if (strpos($docbook_rng, '.rng') !== false ) {
+                $relaxng = $docbook_rng;
+            }
+            $docbook = '<'."?xml version='1.0'?>\n";
+            $docbook .= '<book xmlns="http://docbook.org/ns/docbook" version="5.0">
+<title>My First Book</title><article><title>test</title>
+';
+            $docbook .= $res;
+            $docbook .= '</article></book>';
+
+            file_put_contents('docbook_result.xml', $docbook);
+            $output = array();
+            $returnCode = 0;
+            $cmd = "xmllint --relaxng ".escapeshellcmd($relaxng).' --noout docbook_result.xml 2>&1';
+            exec($cmd, $output, $returnCode);
+            $this->assertEquals("docbook_result.xml validates", implode("\n", $output));
+        }
+
     }
 }
 
