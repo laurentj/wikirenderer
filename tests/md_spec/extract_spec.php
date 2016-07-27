648d2c932b5eb518524ac2c8b1241ee9f41be367
Laurent Jouanneau <dev@ljouanneau.com>
2016-07-27 22:29:11 +0200

    update md specs

--- tests/md_spec/extract_spec.php
+++ tests/md_spec/extract_spec.php
@@ -3,13 +3,16 @@
 // from https://raw.githubusercontent.com/jgm/CommonMark/master/spec.txt
 $content = file_get_contents(__DIR__.'/spec.txt');
 
-
-$parts = preg_split("/(````````````````````````````````( example)?)/m", $content, -1, PREG_SPLIT_DELIM_CAPTURE);
+$separator = "````````````````````````````````";
+$parts = preg_split("/(".$separator."( example)?)/m", $content, -1, PREG_SPLIT_DELIM_CAPTURE);
 
 $k = 1;
-for($i=3; $i < count($parts); $i+=5) {
-    //echo $i.': '.str_replace("\n", '\n', substr($parts[$i], 0, 30))."\n";
-    list($md, $html) = explode("\n.\n", $parts[$i],2);
+for($i=0; $i < count($parts); $i++) {
+    if ($parts[$i] != $separator." example") {
+        continue;
+    }
+    echo ($i+2).': '.str_replace("\n", '\n', substr($parts[$i+2], 0, 30))."\n";
+    list($md, $html) = explode("\n.\n", $parts[$i+2],2);
     $md = str_replace("→", "\t", $md);
     $html = str_replace("→", "\t", $html);
     $md = substr($md, 1); // remove trailing \n
@@ -17,4 +20,5 @@ for($i=3; $i < count($parts); $i+=5) {
     $content = array($md, $html);
     file_put_contents('test_'.($k).'.json', json_encode($content));
     $k++;
+    $i += 4;
 }
